22d77f0f581619a3ebf86cf0de003391a3d8c887
David Saga <davidsa@uia.no>
2022-10-11 23:02:41 +0200

    Add project files.

---
+++ BulkyBook/Models/Kategori.cs
@@ -0,0 +1,12 @@
+﻿namespace BulkyBook.Models
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int displayorder { get; set; }
+        public DateTime CreationTime { get; set; }= DateTime.Now;
+
+    }
+}
+    
